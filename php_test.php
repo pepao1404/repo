ab5fe263d3e82c6afa77b592ed7ec87700b5235a
池谷 祐真 <NC00007018@ncg.local>
2020-02-12 19:26:09 +0900

    セッションつけた

---
+++ php_test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Void</title>
+</head>
+<body>
+    <form action="" method="get">
+        <input type="text" name="tex">
+        <input type="submit" >
+    </form>
+<?php
+require "php_test2.php";
+
+?>    
+</body>
+</html>
